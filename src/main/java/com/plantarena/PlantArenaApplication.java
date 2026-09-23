c43c90ce8f567a00d2f4a427972ff6bcb4b50c6e
Багманов Владимир <vovabag@mts.ru>
2026-09-23 21:15:49 +0300

    feat: maven-скелет монолита (Boot 4.0.8, Java 21, JaCoCo gate 70%)

---
+++ src/main/java/com/plantarena/PlantArenaApplication.java
@@ -0,0 +1,12 @@
+package com.plantarena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantArenaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantArenaApplication.class, args);
+    }
+}
